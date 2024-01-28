69da42f798bbe5ed6b632934f51d8c1b65ec7376
akashtic <81064951+akashtic@users.noreply.github.com>
2024-01-29 01:05:37 +0530

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,8 @@
+function startGame() {
+    window.location.href = "index.html";
+}
+
+function viewHighscore() {
+    // Add logic to view high score
+    alert('High Score: 1000'); // Replace with your high score logic
+}
